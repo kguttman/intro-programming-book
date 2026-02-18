e41bb082593e02776020b4fc9af8b64ab24ec66e
Ken Guttman <kjguttman@gmail.com>
2026-02-18 09:39:24 -0500

    Data Types/Exercise 1 is completed

---
+++ data_types/exercise_1.py
@@ -0,0 +1,23 @@
+# Identify the data type or class for each of the following values:
+
+# Difference between data type and class? data type is a more general 
+# term, e.g. If we say "this is a string", that's a more universal
+# kind of label, i.e. all programming languages have strings 
+# as a data type. But "str", the string class, is where strings 
+# are defined in Python. If you need more info about strings, 
+# you look in the documentation for the "str" class.
+
+# "literals" - things you can type on the screen to 
+# instantly create an object.
+
+# 'True'          = String/str
+# False           = Boolean/bool
+# (1, 2, 3)       = Tuple/tuple
+# 1.5             = Float/float
+# [1, 2, 3]       = List/list
+# 2               = Integer/int
+# range(5)        = Range/range    
+# (above we're invoking range constructor function. Ranges don't have a literal syntax.)
+# {1, 2, 3}       = Set/set
+# None            = NoneType/NoneType
+# {'foo': 'bar'}  = Dictionary/dict
